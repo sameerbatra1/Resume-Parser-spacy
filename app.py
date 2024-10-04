577bf921dde160e3fab402f9592049fc91c7259b
Sameer Batra <sameerbatra1@gmail.com>
2024-10-04 11:17:16 +0500

    adding path

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import spacy
 from spacy import displacy
 
 # Load your pre-trained SpaCy model
-nlp = spacy.load("path/to/your/spacy/model")
+nlp = spacy.load("F:\ml-course\Resume Parser Streamlit app\Resume-Parser-spacy\output\model-best")
 
 def extract_info_from_resume(resume_text):
     doc = nlp(resume_text)
